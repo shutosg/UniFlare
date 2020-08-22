4f392258c80034dddbcd4a143ac75ee9c51129a6
shutosg <shutoreds@gmail.com>
2020-08-23 03:26:15 +0900

    add and rename extensions

---
+++ Packages/net.shutosg.uniflare/Sources/Runtime/Extensions/VectorExtension.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace UniFlare
+{
+    public static class UniFlareVectorExtension
+    {
+        public static Vector3 LerpUnclamped(Vector3 a, Vector3 b, Vector3 factor)
+            => new Vector3(
+                Mathf.LerpUnclamped(a.x, b.x, factor.x),
+                Mathf.LerpUnclamped(a.y, b.y, factor.y),
+                Mathf.LerpUnclamped(a.z, b.z, factor.z)
+            );
+
+        public static Vector3 Clamp(Vector3 a, Vector3 min, Vector3 max)
+            => new Vector3(
+                Mathf.Clamp(a.x, min.x, max.x),
+                Mathf.Clamp(a.y, min.y, max.y),
+                Mathf.Clamp(a.z, min.z, max.z)
+            );
+    }
+}

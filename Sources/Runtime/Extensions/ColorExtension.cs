63d5c56c1f04e9bda7308c08c920f18195b21eb9
shutosg <shutoreds@gmail.com>
2020-08-25 03:27:16 +0900

    update: SetColorHue function

--- Sources/Runtime/Extensions/ColorExtension.cs
+++ Sources/Runtime/Extensions/ColorExtension.cs
@@ -2,16 +2,25 @@
 
 namespace UniFlare
 {
-
     public static class UniFlareColorExtension
     {
         public static Color OffsetHue(this Color self, float hueOffset)
         {
-            Color.RGBToHSV(self, out var h, out var s, out var v);
-            h += hueOffset;
-            var newColor = Color.HSVToRGB(h, s, v);
+            if (Mathf.Approximately(hueOffset, 0f)) return self;
+            var hsv = self.ToHSV();
+            hsv.x += hueOffset;
+            var newColor = hsv.ToRGB();
             newColor.a = self.a;
             return newColor;
         }
+
+        public static Vector3 ToHSV(this Color self)
+        {
+            Color.RGBToHSV(self, out var h, out var s, out var v);
+            return new Vector3(h, s, v);
+        }
+
+        public static Color ToRGB(this Vector3 hsv)
+            => Color.HSVToRGB(hsv.x, hsv.y, hsv.z);
     }
 }

1e0b0cde2535931bcc464ea98fa357b40b961e2e
shutosg <shutoreds@gmail.com>
2020-08-29 18:25:49 +0900

    move flicker function to ValueFlicker class

---
+++ Packages/net.shutosg.uniflare/Sources/Runtime/Tools/ValueFlicker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace UniFlare.Tools
+{
+    public class ValueFlicker
+    {
+        public float Value
+        {
+            get
+            {
+                if (Time.time == _time) return _value;
+                _value = getPerlinNoise() * (Max - Min);
+                _time = Time.time;
+                return _value;
+            }
+        }
+        public float TimeOffset { get; set; }
+        public float Speed { get; set; }
+        public float Min { get; set; }
+        public float Max { get; set; }
+        private float _time = default;
+        private float _value = default;
+
+        public ValueFlicker(float timeOffset = 0, float speed = 1, float min = 0, float max = 1)
+        {
+            TimeOffset = timeOffset;
+            Speed = speed;
+            Min = min;
+            Max = max;
+        }
+
+        private float getPerlinNoise() => Mathf.PerlinNoise((Time.time + TimeOffset) * Speed, 0);
+    }
+}

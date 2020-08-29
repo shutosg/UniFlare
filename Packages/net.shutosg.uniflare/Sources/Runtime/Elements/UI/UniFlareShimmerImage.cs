b6af2752db00f0c917bfbb3efe0c8d112a5040b1
shutosg <shutoreds@gmail.com>
2020-08-29 21:24:43 +0900

    add: new class for shimmer element

---
+++ Packages/net.shutosg.uniflare/Sources/Runtime/Elements/UI/UniFlareShimmerImage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace shutosg.UniFlare.Elements.UI
+{
+    public class UniFlareShimmerImage : UniFlareImageElement
+    {
+        [SerializeField] private float _complexity = 100f;
+
+        public override void UpdateOtherParams()
+        {
+            Image.FlareParam1 = _complexity / 50;
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            UpdateOtherParams();
+        }
+#endif
+    }
+}
